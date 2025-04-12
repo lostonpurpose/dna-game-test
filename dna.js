d4dc19f1c3803d350943dc8366c4b0840fa2768f
Will VanderWyden <vanderwyden@gmail.com>
2025-04-12 17:43:10 +0900

    logic sort of works

--- dna.js
+++ dna.js
@@ -1,24 +1,36 @@
-const colordDom = "blue";
-const colorRec = "yellow";
+window.addEventListener("DOMContentLoaded", () => {
+    
+    const colordDom = "blue";
+    const colorRec = "yellow";
 
-const gen2one = document.getElementById("2-gen-1");
-const gen2two = document.getElementById("2-gen-2");
-const gen2three = document.getElementById("2-gen-3");
-const gen2four = document.getElementById("2-gen-4");
+    const gen2one = document.getElementById("2-gen-1");
+    const gen2two = document.getElementById("2-gen-2");
+    const gen2three = document.getElementById("2-gen-3");
+    const gen2four = document.getElementById("2-gen-4");
 
-const secondGen = document.querySelectorAll(".second");
+    const secondGen = document.querySelectorAll(".second");
 
-function determineColor() {
-    let randomizer = (Math.floor(Math.random() * 4 )  + 1);
-    if (randomizer === 1) {
-        return colorRec;
-    }
-    else {
-        return colordDom;
-    }
+    function determineColor() {
+        let randomizer = (Math.floor(Math.random() * 4 )  + 1);
+        if (randomizer === 1) {
+            return colorRec;
+        }
+        else {
+            return colordDom;
+        }
+    };
 
-};
+    // gen2one.classList.add("color-dom")
 
-console.log(determineColor());
+    secondGen.forEach((child) => {
 
-gen2one.classList.add("color-dom")
+        let getColor = determineColor();
+
+        if (getColor === colorRec) {
+            child.classList.add("color-rec");
+        }
+        else {
+            child.classList.add("color-dom");
+        }
+    });
+});
